c792c171a87dccef063e09bb3df0924fd4545d46
sanket <sanketmaheshwari96@gmail.com>
2025-03-11 00:48:33 +0530

    added display result related to Chat bot with tool

--- src/langgraphagenticai/ui/streamlitui/display_result.py
+++ src/langgraphagenticai/ui/streamlitui/display_result.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_core.messages import HumanMessage,AIMessage
+from langchain_core.messages import HumanMessage,AIMessage,ToolMessage
 import json
 
 
@@ -10,7 +10,7 @@ class DisplayResultStreamlit:
         self.user_message = user_message
 
     def display_result_on_ui(self):
-        usecase = self.usecase
+        usecase= self.usecase
         graph = self.graph
         user_message = self.user_message
         if usecase =="Basic Chatbot":
@@ -22,3 +22,21 @@ class DisplayResultStreamlit:
                             st.write(user_message)
                         with st.chat_message("assistant"):
                             st.write(value["messages"].content)
+
+        elif usecase=="Chatbot with Tool":
+             # Prepare state and invoke the graph
+            initial_state = {"messages": [user_message]}
+            res = graph.invoke(initial_state)
+            for message in res['messages']:
+                if type(message) == HumanMessage:
+                    with st.chat_message("user"):
+                        st.write(message.content)
+                elif type(message)==ToolMessage:
+                    with st.chat_message("ai"):
+                        st.write("Tool Call Start")
+                        st.write(message.content)
+                        st.write("Tool Call End")
+                elif type(message)==AIMessage and message.content:
+                    with st.chat_message("assistant"):
+                        st.write(message.content)
+             
